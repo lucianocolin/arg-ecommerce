360d52b1ae44d0c4e0f3a6b6203774178a154f22
lucianocolin <lucianocolin99@gmail.com>
2023-01-29 16:13:47 -0300

    general validation

--- arg-ecommerce-react/src/pages/Contact.jsx
+++ arg-ecommerce-react/src/pages/Contact.jsx
@@ -17,6 +17,7 @@ const Contact = () => {
   }
   
   const [form, setForm] = useState(initialFormValues)
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const { name, lastname, country, mail, phone } = form;
 
@@ -26,6 +27,21 @@ const Contact = () => {
       [e.target.name]: e.target.value
     })
   };
+
+  const handleFocus = () =>{
+    setErrorMsg(null);
+  };
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if (name === '' || lastname === '' || country === '' || mail === '' || phone === 0){
+      setErrorMsg('Todos los campos son obligatorios');
+      return;
+    }
+    setErrorMsg(null);
+    setForm(initialFormValues);
+    console.log(form);
+  }
   
   return (
     <>
@@ -33,7 +49,7 @@ const Contact = () => {
       <h4 className='contact-second-title'>
         Dejanos tus datos para que podamos contactarnos
         con vos!</h4>
-      <Form className='contact-form' >
+      <Form className='contact-form' onSubmit={handleSubmit} >
         <Form.Group>
           <Form.Label>Nombre</Form.Label>
           <Form.Control
@@ -42,7 +58,8 @@ const Contact = () => {
             placeholder='Colocá tus nombres'
             name='name'
             value={name}
-            onChange={handleChange}  >
+            onChange={handleChange}
+            onFocus={handleFocus}  >
           </Form.Control>
         </Form.Group>
 
@@ -55,7 +72,7 @@ const Contact = () => {
             name='lastname'
             value={lastname}
             onChange={handleChange}
-               >
+            onFocus={handleFocus}  >
           </Form.Control>
         </Form.Group>
 
@@ -67,14 +84,16 @@ const Contact = () => {
             placeholder='¿En qué país estas?'
             name='country'
             value={country}
-            onChange={handleChange}  >
-              <option value="arg">Argentina</option>
-              <option value="bra">Brasil</option>
-              <option value="ch">Chile</option>
-              <option value="col">Colombia</option>
-              <option value="ecu">Ecuador</option>
-              <option value="pe">Perú</option>
-              <option value="uru">Uruguay</option>
+            onChange={handleChange}
+            onFocus={handleFocus}  >
+              <option value="">--¿En qué país estás?--</option>
+              <option value="argentina">Argentina</option>
+              <option value="brasil">Brasil</option>
+              <option value="chile">Chile</option>
+              <option value="colombia">Colombia</option>
+              <option value="ecuador">Ecuador</option>
+              <option value="peru">Perú</option>
+              <option value="uruguay">Uruguay</option>
           </Form.Select>
         </Form.Group>
 
@@ -86,7 +105,8 @@ const Contact = () => {
             placeholder='Colocá tu correo electrónico'
             name='mail'
             value={mail}
-            onChange={handleChange}  >
+            onChange={handleChange}
+            onFocus={handleFocus}  >
           </Form.Control>
         </Form.Group>
 
@@ -97,9 +117,14 @@ const Contact = () => {
             type='phone'
             name='phone'
             value={phone}
-            onChange={handleChange}  >
+            onChange={handleChange}
+            onFocus={handleFocus}  >
           </Form.Control>
         </Form.Group>
+
+        {
+          errorMsg ? <p className='contact-form-error-msg'>{errorMsg}</p> : null
+        }
 
         <div className='contact-form-btn-section'>
           <Button type='submit' size='lg'>Enviar</Button>
